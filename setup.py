87a06dbd2c49141f8d48198429ac9b1cb0511c6a
zjensen262 <31410065+zjensen262@users.noreply.github.com>
2021-04-09 15:37:19 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="osda_gen",
+    version="1.0",
+    description="Recurrent Neural Network to generate OSDAs",
+    url="https://github.com/olivettigroup/OSDA_Generator",
+    author="Zach Jensen",
+    author_email="zjensen@mit.edu",
+    license="MIT",
+    install_requires=install_requires=[
+        "numpy          == 1.16.5",
+        "h5py           == 2.9.0",
+        "tensorflow-gpu == 2.0.0",
+        "tqdm           == 4.35.0",
+        "scikit-learn   == 0.21.3",
+        "scipy          == 1.3.1",
+        "ipykernel      == 5.1.1",
+        "ipython",
+        "matplotlib     == 3.1.1",
+        "pandas         == 0.25.1",
+	"molsets        == 0.2"
+    ],
+    zip_safe=False,
+)
